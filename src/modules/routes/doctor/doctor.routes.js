a73989e36e33d9f1216c023813cd8a24ca7e1745
Luis08089 <fernandomartinez@morpheusdss.com>
2022-03-07 10:58:53 -0600

    cambios3

---
+++ src/modules/routes/doctor/doctor.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+
+
+module.exports = () => {
+    const router = Router();
+
+    router.put('/insert', (req,res) => {
+
+    });
+    return router;
+}
--- src/shared/Middlewares/index.middlewares.js
+++ src/shared/Middlewares/index.middlewares.js
@@ -2,14 +2,23 @@ const express = require('express');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const morgan = require('morgan');
+const compression = require('compression');
+
+const validateJsonPayloadMiddleware = require('../Middlewares/validateJsonPayload.middleware');
+const errorHandlerMiddleware = require('../Middlewares/errorHandler.middleware');
 
 module.exports = ({ applicationServer }) => {
     const middlewares = [
-        cors(),
+        cors({origin: 'http://localhost', credentials: true}),
         helmet(),
         express.json(),
         express.urlencoded({ extended: false }),
         cookieParser(),
+        compression(),
+        morgan('dev'),
+        validateJsonPayloadMiddleware(),
+        errorHandlerMiddleware()
     ];
     middlewares.forEach((x) => applicationServer.use(x));
 };
